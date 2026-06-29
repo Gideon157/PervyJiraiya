16c45c2072dc9d02e847377fc8bb585abd112928
Dario D'Agostino <dario.dagostino157@gmail.com>
2026-06-30 01:01:40 +0200

    Debug R2 vars

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const { generateSignedUrl } = require("./config/r2");
 const app = express();
 const DOMAIN = process.env.DOMAIN_URL;
 console.log("DOMAIN =", DOMAIN);
+console.log("R2_BUCKET =", process.env.R2_BUCKET);
+console.log("R2_ENDPOINT =", process.env.R2_ENDPOINT);
+console.log("R2_ACCESS_KEY_ID =", !!process.env.R2_ACCESS_KEY_ID);
+console.log("R2_SECRET_ACCESS_KEY =", !!process.env.R2_SECRET_ACCESS_KEY);
 
 app.use(cors());
 app.use(express.json());
